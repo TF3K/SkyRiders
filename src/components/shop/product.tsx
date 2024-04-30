05fb38acfa1cea151f4bcd7fbda7e08f0802e988
TF3K <chigivaralopez1010@gmail.com>
2024-04-30 11:56:35 +0100

    component update

--- src/components/shop/product.tsx
+++ src/components/shop/product.tsx
@@ -1,4 +1,5 @@
 import Link  from "next/link"
+import Image from "next/image"
 
 export function Product(props: {
     data: {
@@ -13,9 +14,9 @@ export function Product(props: {
 
         <div className="bg-white rounded-lg overflow-hidden shadow-2xl xl:w-1/5 lg:w-1/4 md:w-1/3 sm:w-1/2 mx-5 mb-10">
             <Link href={`/shop/product/${props.data.id}`}>
-                <img className="h-48 w-full object-cover object-end"
-                     src={props.data.image}
-                     alt="Home in Countryside"/>
+                <Image className="h-48 w-full object-cover object-end"
+                    src={props.data.image}
+                    alt="Home in Countryside"/>
                 <div className="p-6">
                     <div className="flex items-baseline">
                         <span
